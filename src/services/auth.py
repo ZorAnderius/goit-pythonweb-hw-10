6f1a2ab0077c8f8b9e0c75088676de8affcc4ab8
ZorAnderius <gabriel.zorander@gmail.com>
2024-12-06 19:49:33 +0000

    add hash class for JWT auth and authenticate

---
+++ src/services/auth.py
@@ -0,0 +1,59 @@
+from datetime import datetime, timedelta, UTC
+from typing import Optional
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from passlib.context import CryptContext
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.conf.config import config
+from src.database.db import get_db
+from src.database.models import User
+from src.services.users import UserService
+
+
+class Hash:
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    def verify_password(self, plain_password, hashed_password) -> bool:
+        return self.pwd_context.verify(plain_password, hashed_password)
+
+    def get_password_hash(self, password: str) -> str:
+        return self.pwd_context.hash(password)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
+
+async def create_access_token(data: dict, expires_delta: Optional[int] = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(UTC) + timedelta(seconds=expires_delta)
+    else:
+        expire = datetime.now(UTC) + timedelta(seconds=config.JWT_EXPIRATION_TIME)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, config.JWT_SECRET_KEY, algorithm=config.JWT_ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: str = Depends(oauth2_scheme),
+                           db: AsyncSession = Depends(get_db)) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token,
+                             config.JWT_SECRET_KEY,
+                             algorithms=[config.JWT_ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError as e:
+        raise credentials_exception
+
+    user_service = UserService(db)
+    user = await user_service.get_user_by_username(username)
+    if user is None:
+        raise credentials_exception
+    return user
