51f63cba98c9833f1f4dcdca78656353e167a408
ZorAnderius <gabriel.zorander@gmail.com>
2024-12-06 22:28:39 +0000

    add limit request for current user route

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from slowapi.errors import RateLimitExceeded
+from starlette.responses import JSONResponse
 
 from src.api import utils, contacts, auth, users
 
 app = FastAPI()
+
+@app.exception_handler(RateLimitExceeded)
+async def rate_limit_exception_handler(request: Request,
+                                       exc: RateLimitExceeded):
+    return JSONResponse(status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+                        content={"detail": "Too many requests"})
 app.include_router(utils.router, prefix='/api')
 app.include_router(contacts.router, prefix='/api')
 app.include_router(auth.router, prefix='/api')
--- src/api/users.py
+++ src/api/users.py
@@ -1,12 +1,16 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
+from slowapi import Limiter
+from slowapi.util import get_remote_address
 
 from src.database.models import User
 from src.schemas import UserResponse
 from src.services.auth import get_current_user
 
 router = APIRouter(prefix="/users", tags=["user"])
+limiter = Limiter(key_func=get_remote_address)
 
 @router.get('/me', response_model=UserResponse,
-            response_description="Get current user info")
-async def get_current_user(user: UserResponse = Depends(get_current_user)) -> User:
+            response_description="Get current user info (no more than 10 requests per minute)")
+@limiter.limit("10/minute")
+async def get_current_user(request: Request, user: UserResponse = Depends(get_current_user)) -> User:
     return user
