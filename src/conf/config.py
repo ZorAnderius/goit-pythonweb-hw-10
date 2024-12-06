5bb8554ebaf55499b8ca42245b410dd6e63959ad
ZorAnderius <gabriel.zorander@gmail.com>
2024-12-06 19:26:38 +0000

    add JWT config

--- src/conf/config.py
+++ src/conf/config.py
@@ -5,5 +5,8 @@ load_dotenv('.env')
 
 class Config:
     DB_URL = f"postgresql+asyncpg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@postgres:5432/{os.getenv('POSTGRES_DB')}"
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_ALGORITHM = os.getenv('JWT_ALGORITHM')
+    JWT_EXPIRATION_TIME = os.getenv('JWT_EXPIRATION_TIME')
 
 config = Config
